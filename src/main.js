b28b2d7d9e48879cf8b19740aa4d37f71e655fbe
高柯楠 <gaokenan@gaokenans-MacBook-Air.local>
2018-01-15 00:03:46 +0800

    add back to top, update music panel style

--- src/main.js
+++ src/main.js
@@ -62,4 +62,27 @@ $(function() {
         $('.music-switch').removeClass('move-left');
         $('#wrapper-music').removeClass('show-music-list');
     });
+
+
+    // hide #back-top first
+    $("#back-top").hide();
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
 });
